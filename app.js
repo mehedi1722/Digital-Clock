e693bfa1b507df8ffb58dd7b1a0bb5085ebd462b
mehedi1722 <mh514928@gmail.com>
2021-07-12 21:38:50 +0600

    Complete Done

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const hour = document.getElementById('hour');
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const progress = document.getElementById('progress');
-
 function MyWatch(){
+    // Time Display Fields
+    const hour = document.getElementById('hour');
+    const minute = document.getElementById('minute');
+    const second = document.getElementById('second');
+    const progress = document.getElementById('progress');
+    
+    // Get time 
     let date = new Date();
     let Hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
+    // Conditions for time formate 
     hour.textContent = Hour < 10 ? ("0" + Hour) : Hour
     minute.textContent = min < 10 ? ("0" + min) : min;
     second.textContent = sec < 10 ? ("0" + sec) : sec;
 
     progress.style.width = (sec / 60) * 100 + "%"; 
 }
+
 setInterval(MyWatch, 1000);
